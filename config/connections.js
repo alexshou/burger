f72ea5de93ef843d99922827585a961eb4bf3175
Xingxian Shou <Xingxian Shou>
2017-08-10 11:25:36 -0400

    add JAWSDB_URL setting

--- config/connections.js
+++ config/connections.js
@@ -4,13 +4,17 @@ const MySQL = require('mysql');
 Promises.promisifyAll(require("mysql/lib/Connection").prototype);
 Promises.promisifyAll(require("mysql/lib/Pool").prototype);
 
-let connection = MySQL.createConnection({
-	host: "us-cdbr-iron-east-05.cleardb.net",
+if(process.env.JAWSDB_URL) {
+	connection = MySQL.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = MySQL.createConnection({
+	host: "localhost",
 	port: 3306,
-	user: "b38df6daa5ddb2",
-	password: "5154e53b",
-	database: "heroku_7f58e5ffafab8cd"
+	user: "root",
+	password: "",
+	database: "burgers_db"
 });
+}
 
 let allConnections = {
 	'burger' : connection
@@ -19,3 +23,9 @@ let allConnections = {
 module.exports = allConnections;
 
 // mysql://b38df6daa5ddb2:5154e53b@us-cdbr-iron-east-05.cleardb.net/heroku_7f58e5ffafab8cd?reconnect=true
+
+	// host: "localhost",
+	// port: 3306,
+	// user: "root",
+	// password: "",
+	// database: "burgers_db"
